07b90e70c676a1dc95dd0ed5b5e079ceb711b467
RedaTargaoui <redatarg@gmail.com>
2024-08-08 16:26:21 +0200

    Update GUI.java

--- src/main/java/com/redat/GUI.java
+++ src/main/java/com/redat/GUI.java
@@ -10,7 +10,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 public class GUI extends JFrame {
